26d0b8d9f0ba3f88c244d175b532b61d653f7228
Daniel-Marty <daniel.n.please@gmail.com>
2022-08-13 11:33:35 +0300

    Default response 1

--- js/hey-listen.js
+++ js/hey-listen.js
@@ -19,7 +19,7 @@ const debounce = (fn, delay) => {
         }, delay)
     }
 }
-
+var transcript = '';
 // ==============================        MICRO              ===============================
 
 microphone.addEventListener('click', function () {
@@ -28,9 +28,10 @@ microphone.addEventListener('click', function () {
 })
 recognition.onresult = function (e) {
     console.log(e);
-    var transcript = e.results[0][0].transcript;
+    transcript = e.results[0][0].transcript;
     transcript.innerHTML = transcript;
     textarea.innerHTML = transcript;
+
 }
 // speak.addEventListener('click', function () {
 //     speak.style.background = 'red';
@@ -96,8 +97,9 @@ function sayItBitch(answer) {
 let KW = '';
 inputText.addEventListener('input', () => {
     // transcript = input.value.toUpperCase();
-    transcript = input.value;
+    // transcript = input.value;
     textarea.innerHTML = input.value;
+
 })
 inputText.addEventListener('input', debounce(e => {
     KW = `${textarea.innerHTML}`
@@ -105,20 +107,94 @@ inputText.addEventListener('input', debounce(e => {
     console.log(KW);
 }, 1000));
 
-// let KW = ''
-// function (word) {
-//     keyWords.includes(word)
-//     KW = word;
-// }
 
-// function setKeyWords(catchWords) {
-//     if (textarea.innerHTML.includes(`${catchWords}`))
-//         keyWords = catchWords;
-//     console.log(keyWords);
-// } 
 // =======================================DIALOGUE FUNCTIONS=======================================
+let currentArray = '';
+let defaultArray = [];
+let greetingsArray = ["what's up", "oh, hello, how have you been?", "what's up, player", "hey, how are you?", "hi there", "hey there", "hey, how is it going?", "oh, hello there", "yes, hello", "greetings to you", "make it rain, let's go!", "wow, you're so brave, hi, let's talk",];
+let weatherArray = ["Why don't you ask Siri about it?", "Just look outside and you'll know", "Open the window and stick out your hand to feel it yourself", "I think it's cloudy", "I have no idea", "I haven't the foggiest idea",];
+
+function getRandoms(array) {
+    random_answer = Math.floor(Math.random() * array.length);
+    testRandomNumber = array[random_answer];
+    sayItBitch(array[random_answer]);
+    // array.splice(random_answer, 1);
+};
+function defaultResponse() {
+    o1 = `Did you say ${KW}I'm not sure I have an answer for that`;
+    o2 = `Hold up, did you just say${KW}`;
+    o3 = `What do you want me to respond to ${KW}`;
+    o4 = `Can you say something better than${KW}?`;
+    o5 = `Don't ask me to respond to ${KW}`;
+    o6 = `Well, I'm not sure about that, but did you say${KW}?`;
+    o7 = `What do you mean by${KW}?`;
+    o8 = `Tell your mom about ${KW}?`;
+    o9 = `Hold on, I'm gonna need some help with${KW}, could you expand on it please?`;
+    o10 = `Do you think it's encouraging to hear${KW}from you?`;
+    o11 = `I'm pretty sure you can do better than${KW}`;
+    o12 = `${KW}? Are you serious?`;
+    o13 = `${KW}This is exactly what I told your mom when we broke up`;
+    o14 = `${KW} - My dog told me the same thing last night`;
+    o15 = `You've just said ${KW}, well that's pretty sad`;
+    o16 = `${KW}, um... try again`;
+    o17 = `Um...${KW}? This is not what I'd like to talk to you about`;
+    o18 = `${KW}? Um... I don't get it, but nice try anyway`;
+    o19 = `Um... Do you say'${KW}'to everybody?`;
+    o20 = `${KW}, how interesting`;
+    o21 = `${KW}? Tell me more about it`;
+    o22 = `${KW} - This is exactly what I said at my cat's funeral`;
+    o23 = `Wow! ${KW}, really? I need to analyze this for a moment`;
+    o24 = `My brain is going to explode if I hear '${KW}' again`
+
+
+
+    defaultArray = [o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12, o13, o14, o15, o16, o17, o18, o19, o20, o21, o22, o23, o24,]
+    getRandoms(defaultArray);
+}
 function keyWordsCheck() {
     if (KW.includes('hello') || KW.includes('hi') || KW.includes('morning') || KW.includes('afternoon') || KW.includes('evening')) {
-        sayItBitch("What's up");
-    }
-}
+        getRandoms(greetingsArray);
+    } else if ((KW.includes('how') || KW.includes('what') || KW.includes('tell')) && KW.includes('weather')) {
+        getRandoms(weatherArray);
+    } else if ((KW.includes('my') || KW.includes("I'm") || KW.includes('tell')) && KW.includes('name')) {
+        getRandoms(weatherArray);
+    } else { defaultResponse() }
+};
+
+
+
+
+
+// function testCallback(question, who, connection, mark1, verb, style, time, sentence) {
+//     questionWord.innerHTML = `${question}`
+//     changeSubject(who);
+//     connectionWord.innerHTML = `${connection}`
+//     changeMark(mark1)
+//     verbPic.src = `${verbsFolder}${verb}`
+//     changeBodyStyle(style);
+//     changeVerb(verb);
+//     timeMarker.innerHTML = `${time}`
+//     textSolution.innerHTML = `${sentence}`
+//     currentSentence = sentence;
+//     if (mark1 === "1.png") {
+//         fuckingShit = '1';
+//     } else { fuckingShit = '2' }
+// };
+// function generateSentence() {
+//     if (currentArray === ' ') {
+//         getRandoms(randomNumbersAll);
+//     } else if (currentArray === 'greetings') {
+//         getRandoms(presentArray);
+//     } else if (currentArray === 'Past') {
+//         getRandoms(pastArray);
+//     } else if (currentArray === 'Future') {
+//         getRandoms(futureArray)
+//     } else if (currentArray === 'questions') {
+//         getRandoms(questionsArray)
+//     } else if (currentArray === 'negatives') {
+//         getRandoms(negativesArray)
+//     } else {
+//         getRandoms(affirmativeArray)
+//     }
+//     testSimple();
+// }
